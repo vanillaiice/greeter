c3d3fcb7b23b53f516964fd5bd83154aa63da3ea
vanillaiice <foofoobarr@proton.me>
2022-12-16 22:01:42 +0300

    randomized greeting prompt

--- greeter.v
+++ greeter.v
@@ -1,8 +1,10 @@
 module main
 
 import os { input }
+import rand { int_in_range }
 
 fn main() {
+	greetings := ["Hi", "Hello", "Hey", "What's up", "Sup", "Yo"]
 	mut name := input('What is your name: ')
-	println('Hello, $name !')
+	println('${greetings[int_in_range(0, greetings.len - 1)!]}, $name !')
 }
